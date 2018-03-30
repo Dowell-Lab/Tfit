1a4c63355dbf9b509a82f0371b4e916df4aeeae4
migo0123 <michael.gohde@colorado.edu>
2018-03-30 14:05:23 -0600

    Added ParamWrapper stub and fixed a spelling mistake.

---
+++ src/ParamWrapper.hpp
@@ -0,0 +1,14 @@
+#ifndef PARAMWRAPPER_HPP
+#define PARAMWRAPPER_HPP
+#include <cstdio>
+#include <string>
+#include <cstlib>
+#include <map>
+#include <vector>
+
+/* This class is similar to the ParamWrapper used by FStitch. It sgoal is to make it easier to
+ * change, add, and replace command line options in Tfit. 
+ */
+ 
+ 
+#endif 
