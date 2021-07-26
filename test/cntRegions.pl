a7026a3e61768d87e29756c78b36a88ce35c2ec9
dowellde <robin.dowell@colorado.edu>
2021-07-26 11:38:13 -0600

    Removing the test directory -- this shouldn't be in the repository but just locally.

--- test/cntRegions.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-my $cnt = 0;
-while(<>) {
-  if (! /^#/) {$cnt++; }
-}
-print $cnt ."\n";
--- test/runTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-date
-mpirun -np 1 ../src/Tfit model -v -config ../config_files/config_file.txt -ij ../../../data/PRO-BSA-Eric-1.bedGraph_typical_region.bg -N testmodel -k singleregion.bed -o .
-date
