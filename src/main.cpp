865c10d4a1a3b90cca259ddc6ee273d9a35e0c64
dowellde <robin.dowell@colorado.edu>
2022-01-17 14:59:34 -0700

    cpplint check -- corrected

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,17 @@
  */
 
 #include <errno.h>
-#include <math.h>   
-#include <stdio.h>   
-#include <time.h>  
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <mpi.h>
+#include <omp.h>
 
-#include <chrono>
+#include <chrono>  // This is an unapproved C++11 header
 #include <iostream>
 #include <limits>
 #include <map>
-#include <thread>
-
-#include <mpi.h>
-#include <omp.h>
+#include <thread>  // This is an unapproved C++11 header
 
 #include "across_segments.h"
 #include "bidir_main.h"
@@ -37,8 +36,6 @@
 #include "select_main.h"
 #include "template_matching.h"
 
-using namespace std;
-
 /**
  * @brief Program main.  
  * It contains the mpi code, reading parameters, then forking to one of three modules. 
@@ -46,48 +43,49 @@ using namespace std;
  * @param argv
  * @return 
  */
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
   MPI::Init(argc, argv);
   // the nprocs is the total number of processors available
-  int nprocs		= MPI::COMM_WORLD.Get_size();
+  int nprocs  = MPI::COMM_WORLD.Get_size();
   // The rank ranges from 0 to (nprocs-1) and is current process number
-  int rank 		= MPI::COMM_WORLD.Get_rank();
-  int threads  	= omp_get_max_threads();
+  int rank    = MPI::COMM_WORLD.Get_rank();
+  int threads = omp_get_max_threads();
 
-  params * P 	= new params();
+  params * P  = new params();
   read_in_parameters(argv, P, rank);
-  if (P->EXIT){
-    if (rank == 0){
+  if (P->EXIT) {
+    if (rank == 0) {
       printf("exiting...\n");
     }
     delete P;
     MPI::Finalize();
     return 0;
   }
-  int job_ID 		=  MPI_comm::get_job_ID(P->p["-log_out"], P->p["-N"], rank, nprocs);
-  
-  int verbose 	= stoi(P->p["-v"]);
-  Log_File * LG 	= new  Log_File(rank, job_ID, P->p["-N"], P->p["-log_out"]);
-  if (verbose > 0) { 
-     // printf("This should be output when verbose is set!\n"); 
-   } 
- if (verbose and rank==0){ 
-    P->display(nprocs,threads);
-    LG->write(P->get_header(1),0);
+  int job_ID = MPI_comm::get_job_ID(P->p["-log_out"], P->p["-N"], rank, nprocs);
+
+  int verbose   = stoi(P->p["-v"]);
+  Log_File * LG = new  Log_File(rank, job_ID, P->p["-N"], P->p["-log_out"]);
+  if (verbose > 0) {
+  // printf("This should be output when verbose is set!\n");
   }
-  if (P->bidir){
-    bidir_run(P, rank, nprocs, job_ID,LG);
-  } else if (P->model){
-    model_run(P, rank, nprocs, 0, job_ID,LG);
-  }else if (P->select){
-    // This one is commented out -- ie. this function does nothing.
-    select_run(P, rank, nprocs, job_ID,LG);	
+  if (verbose && rank == 0) {
+    P->display(nprocs, threads);
+    LG->write(P->get_header(1), 0);
   }
-  if (rank == 0){
+  if (P->bidir) {
+    bidir_run(P, rank, nprocs, job_ID, LG);
+  } else if (P->model) {
+    model_run(P, rank, nprocs, 0, job_ID, LG);
+  } else if (P->select) {
+    // Contents of select_run are commented out:
+    // i.e. this function does nothing.
+    select_run(P, rank, nprocs, job_ID, LG);
+  }
+  if (rank == 0) {
     load::collect_all_tmp_files(P->p["-log_out"], P->p["-N"], nprocs, job_ID);
   }
-  
+
   MPI::Finalize();
-  
+
   return 0;
 }
