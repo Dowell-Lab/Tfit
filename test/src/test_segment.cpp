d8471a03bcc0ff1c212f120890fff3b8a310c964
dowellde <robin.dowell@colorado.edu>
2022-02-01 13:28:12 -0700

    pulled all src into testing harness

---
+++ test/src/test_segment.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file test_segment.cpp 
+ * @author Robin Dowell
+ * @brief Testing the old segment class (to be refactored) 
+ * @date 2022-02-01
+ * 
+ */
+#include "gmock/gmock.h"
+#include "load.h"  // contains segment class
+
+TEST(segment, LengthEquivalent)
+{
+    // Arrange: bring SUT to desired state
+    segment sut = segment("chrTest", 100, 1000, 3, "+"); 
+
+    // Act: call methods on SUT, capture output
+    double length = sut.getXLength(); 
+    double oldLength = sut.maxX - sut.minX;
+
+    // Assert: Verify the outcome
+    EXPECT_EQ(length, oldLength);
+}
