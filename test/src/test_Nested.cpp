430a4575f98b15950eb6b95fb98e9225d3791851
dowellde <robin.dowell@colorado.edu>
2022-02-03 09:34:09 -0700

    debugging nested calls test infrastructure

---
+++ test/src/test_Nested.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file test_segment.cpp 
+ * @author Robin Dowell
+ * @brief Testing the old segment class (to be refactored) 
+ * @date 2022-02-01
+ * 
+ */
+#include "gmock/gmock.h"
+#include "load.h"  // contains segment class
+
+TEST(nested, reproduceNested)
+{
+  // Arrange: bring SUT to desired state
+  string file_name = "../testNested.K-models.tsv"; 
+  params * P  = new params();
+
+  vector<segment_fits *> fits 		= load::load_K_models_out(file_name);
+  load::write_out_bidirectionals_ms_pen(fits, P, 2, 1);
+
+  // Act: call methods on SUT, capture output
+
+  // Assert: Verify the outcome
+  
+}
