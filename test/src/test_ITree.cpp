eb50f22efaa02c7fd3ccc54fdeb663ea21e110d6
dowellde <robin.dowell@colorado.edu>
2022-03-18 15:58:41 -0600

    skeleton test for ITree

---
+++ test/src/test_ITree.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file test_ITree.cpp 
+ * @author Robin Dowell
+ * @brief Testing the centered Interval Tree
+ * @date 2022-03-18
+ * 
+ */
+#include "gmock/gmock.h"
+#include "ITree.h"
+
+TEST(ITree, basics)
+{
+    // Arrange: bring SUT to desired state
+
+    // Act: call methods on SUT, capture output
+
+    // Assert: Verify the outcome
+}
