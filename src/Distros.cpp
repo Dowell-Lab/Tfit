4c59daf2ecbdfabbbe229064ac6bee79f239a8cf
dowellde <robin.dowell@colorado.edu>
2022-05-06 12:39:26 -0600

    staging rewrite of model.cpp

---
+++ src/Distros.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file Distros.cpp
+ * @author Robin Dowell 
+ * @brief Contains the basic mathematical distribution functions for the
+ * Normal, Exponential, EMG, and Uniform
+ * @version 0.1
+ * @date 2022-05-22
+ * 
+ */
+#include "Distros.h"
+
+// Classes for Uniform, Expoential, EMG, Normal, and such go here.
---
+++ src/Distros.h
@@ -0,0 +1,16 @@
+/**
+ * @file Distros.h
+ * @author Robin Dowell 
+ * @brief Mathematical distributions used in the model.
+ * @version 0.1
+ * @date 2022-05-06
+ * 
+ */
+#ifndef Distros_H 
+#define Distros_H 
+
+#include <string>
+
+// Classes for Uniform, Expoential, EMG, Normal, and such go here.
+
+#endif
