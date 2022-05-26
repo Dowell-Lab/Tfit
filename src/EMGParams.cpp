633a38fedbff941ceaa84290fcbb5cda6160fc36
dowellde <robin.dowell@colorado.edu>
2022-05-26 13:50:33 -0600

    Rename single_model to EMGParams and consolidate tests

---
+++ src/EMGParams.cpp
@@ -0,0 +1,225 @@
+/**
+ * @file EMGParams.cpp
+ * @author Robin Dowell 
+ * @brief Routines necessary for loading data and segments.
+ * @version 0.1
+ * @date 2022-02-08
+ * 
+ */
+#include "EMGParams.h"
+
+#include <math.h>   
+#include <stdio.h>
+#include <unistd.h>
+
+#include <string>
+#include <vector>
+#include <iostream>  // for cout?
+
+#include "split.h"
+
+EMGparameters::EMGparameters() {
+  // Empty constructor.
+  mu = sigma = lambda = pi = footprint = omega = 0.0;    
+}
+
+/**
+ * @brief Construct a new EMGparameters::EMGparameters object
+ * 
+ * @param mu        The center of the bidirectional, position of loading
+ * @param sigma     The variance on the loading position (mu)
+ * @param lambda    The length of the exponential.  EMG = N(mu,sigma) + Exp(lambda)
+ * @param pi        The strand bias
+ * @param footprint     The offset parameter (aka Beta)
+ * @param omega     The pausing probability. 
+ */
+EMGparameters::EMGparameters(double v_mu, double v_sigma, double v_lambda, double v_pi, double v_footprint, double v_omega) {
+    mu = v_mu;
+    sigma = v_sigma;
+    lambda = v_lambda;
+    pi = v_pi;
+    footprint = v_footprint;
+    omega = v_omega;
+}
+
+/**
+ * @brief Convert the parameters into an ouput string.
+ * 
+ * @return std::string 
+ */
+std::string EMGparameters::write() {
+  std::string output =    std::to_string(mu) +"\t" + std::to_string(sigma) +"\t" + 
+    std::to_string(lambda) + "\t" + std::to_string(pi) +"\t" + std::to_string(footprint) 
+    + "\t" + std::to_string(omega) + "\n";
+   return output;
+}
+/**
+ * @brief This is the opposite of the write() function.  Expects
+ * the write() function's output as its input.
+ * 
+ * @param single  Tab delimited list of parameters, 
+ * in order: mu, sigma, lambda, pi, footprint, and omega.
+ */
+void EMGparameters::read(std::string single) {
+   vector<std::string> split_tab = split_by_tab(single);
+
+   mu = std::stod(split_tab[0]);
+   sigma = std::stod(split_tab[1]);
+   lambda = std::stod(split_tab[2]);
+   pi = std::stod(split_tab[3]);
+   footprint = std::stod(split_tab[4]);
+   omega = std::stod(split_tab[5]);
+
+   // Should we check that we don't have more parameters?
+}
+
+/**
+ * @brief The start coordinate for a bed file with this call.
+ * 
+ * @return std::string 
+ */
+double EMGparameters::getStart() {
+    return max(mu-(sigma+lambda), 0.0);
+}
+
+/**
+ * @brief The end coordinate for a bed file with this call.
+ * 
+ * @return std::string 
+ */
+double EMGparameters::getEnd() {
+   return (mu + (sigma+lambda));
+}
+
+/**
+ * @brief Return the parameters as vector of strings.
+ * 
+ * @remark Unclear if this should be concerned with precision in conversion to string
+ * 
+ * @return std::vector<std::string> 
+ */
+std::vector<std::string> EMGparameters::fetch_as_strings() {
+   std::vector<std::string> asStrings(6);
+   // cout << std::to_string(mu) << std::endl;
+   asStrings[0] = std::to_string(mu);
+   asStrings[1] = std::to_string(sigma);
+   asStrings[2] = std::to_string(lambda);
+   asStrings[3] = std::to_string(pi);
+   asStrings[4] = std::to_string(footprint);
+   asStrings[5] = std::to_string(omega);
+
+   return asStrings;
+}
+/******************************************************/
+/* Class: Set_EMGparameters */
+/*************************/
+
+Set_EMGparameters::Set_EMGparameters() {
+   K = 0;
+   log_likelihood = 0;
+}
+
+/**
+ * @brief Construct a new Set_EMGparameters::Set_EMGparameters object
+ * This is a collection of EMGparameters.  Used for a "set of K fits".
+ * 
+ * @param K Number of items in the collection.
+ */
+Set_EMGparameters::Set_EMGparameters(int k) {
+    K = k;
+    EMGparameters *nodata = NULL;
+    for (int i = 0; i < K; i++) {
+       collection.push_back(nodata) ;
+    }
+    log_likelihood = 0;
+  // cout << K << "," << log_likelihood << std::endl;
+}
+/**
+ * @brief Destroy the Set_EMGparameters::Set_EMGparameters object
+ * 
+ */
+Set_EMGparameters::~Set_EMGparameters() {
+   for (int i = 0; i < K; i++) {
+      free(collection[i]);
+   }
+}
+
+/**
+ * @brief Convert a set of distinct fits into something like 
+ * the K_models output string.  NOTE it's not exactly K_models 
+ * as we don't keep the wf and wp parameters here.
+ * 
+ * UNTESTED
+ * 
+ * @return std::string 
+ */
+std::string Set_EMGparameters::write() {
+  std::string asString = "~" + to_string(K) + "," + to_string(log_likelihood);
+
+  // Note that output is currently all K values of mu (et. al.) 
+  // But storage is in K EMGparameters -- this leads to a indexing
+  // issues in the read/write functions.
+  // std::vector<std::vector<std::string>> params(K, std::vector<std::string>(7)); // [models][parameter]
+  std::vector<std::vector<std::string>> params(K);
+  for (int i = 0; i < K; i++) { // each of the K models
+    if (collection[i]) {
+      params[i] = collection[i]->fetch_as_strings();
+      // cout << params[i][0] << std::endl;
+    } else {   // This model is null
+      params[i] = std::vector<std::string>(6,""); // Set to empty strings
+    }
+  }
+
+  for (int j = 0; j < 6; j++) { // each of the parameters
+     std::string temp = params[0][j];
+     // cout << temp << std::endl;
+     for (int i = 1; i < K; i++) {
+       temp = temp + "," + params[i][j];
+     }
+     asString = asString + "\t" + temp;
+  }
+  return asString;
+}
+
+/**
+ * @brief Convert a string of K model output into a Set_EMGparameters 
+ * 
+ * UNTESTED 
+ * 
+ */
+void Set_EMGparameters::read_from_K_models(std::string line) {
+   std::string data = line.substr(1, line.size() - 1); // removes the ~ first character
+   std::vector<std::string> tab_split = split_by_tab(data);
+
+   // These are values for the entire set (size and loglikelihood).
+   std::vector<std::string> comma_split = split_by_comma(tab_split[0], "");
+   K = stoi(comma_split[0]);
+   log_likelihood = stod(comma_split[1]);
+
+   // Now we need to build all the EMGparameter sets, of which there are K.
+   // Recall the string is formatted:
+   // mus+"\t"+sigmas+"\t"+lambdas+"\t" + pis+"\t" + fps+ "\t" + ws
+   std::vector<std::vector<double>> par_as_double(7, std::vector<double>(K)); // [params][model]
+   for (int i = 1; i < 6; i++) { // mu to footprint
+      std::vector<std::string> temp = split_by_comma(tab_split[i], "");
+      for (int j = 0; j < K; j++) { // each of the K models
+         par_as_double[i][j] = stod(temp[j]);
+      }
+   }
+
+   // omega is tab_split[6] but it's got a bar in it so has to have
+   // separate parsing.  But should go into par_as_double[6].
+   // w, fw, rw|w, fw, rw| ...
+   std::vector<std::string> temp = split_by_bar(tab_split[6], "");
+   // Note that temp.size should eq K.
+   for (int i = 0; i < K; i++) {
+      std::vector<std::string> wset = split_by_comma(temp[i], "");
+      par_as_double[6][i] = stod(wset[0]);
+   }
+
+   for (int i = 0; i < K; i++) {
+      collection.push_back(new EMGparameters(par_as_double[1][i], par_as_double[2][i],
+               par_as_double[3][i], par_as_double[4][i], par_as_double[5][i], 
+               par_as_double[6][i]));
+   }
+}
--- test/src/test_EMGparams.cpp
+++ test/src/test_EMGparams.cpp
@@ -1,5 +1,5 @@
 /**
- * @file test_EMGparameters.cpp
+ * @file test_EMGparams.cpp
  * @author Robin Dowell
  * @brief Unit Testing: testing Tfit/src/single_model.cpp \ref EMGparameters
  * @version 0.1
@@ -7,8 +7,7 @@
  * 
  */
 #include "gmock/gmock.h"
-#include "single_model.h"
-
+#include "EMGParams.h"
 
 TEST(EMGparameters, ReadWrite)
 {
@@ -65,4 +64,46 @@ TEST(EMGparameters, fetch_asStrings)
 
     // Teardown
     params.clear();
+}
+
+TEST(Set_EMGparam, read_from_K_models)
+{
+    // Arrange
+    std::string line  = (std::string)"~2,-18405.714702" + "\t" + "57970016,57974849" 
+        + "\t" + "69.438995,37.975287" 
+        + "\t" + "371.734252,408.579080" + "\t"	+ "0.486291,0.586718"
+        + "\t" + "121.206766,250.000000" + "\t" + "0.617766,0.069621,0.000284|0.310201,0.000341,0.000348"
+        + "\t" + "57976184.000000,57976184.000000" + "\t" + "57965508.000000,57965508.000000";
+    Set_EMGparameters sut;
+
+    // Act
+    sut.read_from_K_models(line);
+
+    // Assert
+    EXPECT_EQ(sut.collection[0]->mu, (double)57970016);
+    EXPECT_EQ(sut.collection[1]->mu, (double)57974849);
+    EXPECT_EQ(sut.collection[0]->omega, (double)0.617766);
+    EXPECT_EQ(sut.collection[1]->omega, (double)0.310201);
+
+    // Teardown
+}
+
+TEST(Set_EMGparam, write)
+{
+    // Arrange
+    Set_EMGparameters sut(2);
+    sut.collection[0] = new EMGparameters(45333,65,44,0.345,32,0.5);
+    sut.collection[1] = new EMGparameters(11111,35,22,0.081,11,0.3);
+
+    // Act
+    std::string Toutput = sut.write();
+    std::string output;
+    output  = (std::string) "~2,0.000000\t" + "45333.000000,11111.000000\t" 
+        + "65.000000,35.000000\t" + "44.000000,22.000000\t" "0.345000,0.081000\t"
+        + "32.000000,11.000000\t" + "0.500000,0.300000";
+
+    // Assert
+    EXPECT_TRUE(Toutput == output);
+
+    // Teardown
 }
