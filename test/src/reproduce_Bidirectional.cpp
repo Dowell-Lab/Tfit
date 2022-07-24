6b0ae083fd3f4380ceedcaf13ab405818f9a6854
dowellde <robin.dowell@colorado.edu>
2022-07-24 16:30:24 -0600

    added reproducetest  for both new pdf methods

--- test/src/reproduce_Bidirectional.cpp
+++ test/src/reproduce_Bidirectional.cpp
@@ -12,7 +12,7 @@
 
 
 // Need to test edge cases and negative strand!
-TEST(BidirEQ, reproducePDF)
+TEST(BidirEQ, reproducePDFGrushka)
 {
     // Arrange: bring SUT to desired state
     Bidirectional sut(35., 1.5, 0.25, 0.5, 0.);
@@ -23,6 +23,25 @@ TEST(BidirEQ, reproducePDF)
 
     // Act: call methods on SUT, capture output
     double newresult = sut.pdf(37.,'+');
+    double oldresult = oldmethod.pdf(37., 1);
+
+    // std::cout << to_string(oldresult) + " is now " + to_string(newresult) << std::endl;
+
+    // Assert: Verify the outcome
+    ASSERT_LE(abs(oldresult - newresult), 0.0001);  
+}
+
+TEST(BidirEQ, reproducePDFKalambet)
+{
+    // Arrange: bring SUT to desired state
+    Bidirectional sut(35., 1.5, 0.25, 0.5, 0.);
+    EMG oldmethod(35., 1.5, 0.25, 1, 0.5);
+
+    // std::cout << oldmethod.write() << std::endl;
+    // std::cout << sut.write_out() << std::endl;
+
+    // Act: call methods on SUT, capture output
+    double newresult = sut.pdf_alt(37.,'+');
     double oldresult = oldmethod.pdf(37., 1);
 
     // std::cout << to_string(oldresult) + " is now " + to_string(newresult) << std::endl;
