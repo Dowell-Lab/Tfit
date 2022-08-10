bac50dbc2c7792e22b9eb21b876900b471212b39
dowellde <robin.dowell@colorado.edu>
2022-08-10 15:41:36 -0600

    testing infrastructure to get EMcomponents reproducing

--- test/src/reproduce_EMcomponents.cpp
+++ test/src/reproduce_EMcomponents.cpp
@@ -1,43 +1,50 @@
 /**
- * @file explore_coreEM.cpp 
+ * @file reproduce_EMcomponents.cpp 
  * @author Robin Dowell
- * @brief Exploring the core EM algorithm, not a test so much as a framework 
- * for debugging.
- * @date 2022-02-24
+ * @brief Checking that revised subroutines reproduce the portions of 
+ * the original EM code that are designed to replace.
+ * @date 2022-07-28
  * 
  */
 #include "gmock/gmock.h"
 #include "load.h"  // contains segment_fits class
-#include "model.h" // contains fit2 implementation (classifier object)
+#include "model.h" // contains fit3 implementation (modifiable version of fit2)
 
 #include "Bedfile.h"
+#include "EMalg.h"
 
-/* This is a very heavy "test" -- more like trying to peel away most of Tfit's
-cruft to get to the core Em algorithm (fit2).  */
-TEST(classifier, exerciseFit2)
+/*
+TEST(reproduceEM, dataloading)
 {
   // Lets load a typical small bedgraph file.
   string joint_bedgraph = "../examples/typical_region.bg";    //chr21 33401693 33407411	
 
-  // loading sets up these mappings, we are going to ignore them.
+  // Joey's code
 	map<string, int> chrom_to_ID;
 	map<int, string> ID_to_chrom;
-
-  // The assorted hard coded values are set by defaults in read_in_parameters
   string emptyfilename;
+  cout << "Setup Variables" << std::endl;
 	vector<segment *> 	segments 	= load::load_bedgraphs_total(emptyfilename, 
 			emptyfilename, joint_bedgraph, 25, 100, "chr21", chrom_to_ID, ID_to_chrom );  
+  cout << "Load Joey" << std::endl;
   segment *data = segments[0];  // We just want the first one
   classifier sut = classifier(0, 0.0001, 2000, 0.05, 0, 1, 1, 1, 1, 1, 1, 0);
   double center = 21;
   data->centers.push_back(center);
+  cout << "Pick Segment and Centers" << std::endl;
+
+  Bedgraph genData;
+  genData.load_file(joint_bedgraph,false);
+  cout << "New Method Load Data" << std::endl;
+  EMalg v2model;
 
   // Act on sut (run EM!)
   // cout << "Before: " +  sut.write_classifier_status() << std::endl;
-  int returned = sut.fit3(data, data->centers, 0, 0);    
+  // int returned = sut.fit3(data, data->centers, 0, 0);    
   // cout << "components: " + sut.write_components() << std::endl;
   // cout << "After: " +  sut.write_classifier_status() << std::endl;
 
   // Assert: Verify the outcome
   EXPECT_LE(abs(sut.ll + 218769.31), 0.01);  
 }
+*/
--- test/src/test_Bedfile.cpp
+++ test/src/test_Bedfile.cpp
@@ -14,11 +14,20 @@
 // Bedfile:  Test I/O for bed3, bed4, bed6, bed12 ?
 
 /*
-TEST(Bedfile, ReadWriteEquivalent) {
-
-}
-
-TEST(Bedgraph, ReadWriteEquivalent) {
+TEST(Bedfile, ReadWriteEquivalent) 
+{
 
 }
 */
+
+TEST(Bedgraph, ReadErrorCheck) 
+{
+    std::string filename = "../examples/typical_region.bg";    //chr21 33401693 33407411	
+    Bedgraph sut;
+
+    sut.load_file(filename, false);
+
+    sut.reportBedfileContents();
+
+    EXPECT_TRUE(true);
+}
