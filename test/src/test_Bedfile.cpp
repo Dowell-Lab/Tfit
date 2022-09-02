fc9a2b34c034f8a705caf7b2fc6c3ccdf9db1d1a
dowellde <robin.dowell@colorado.edu>
2022-09-02 10:44:49 -0600

    Testing IO of bedfiles, with cleanup

--- test/src/test_Bedfile.cpp
+++ test/src/test_Bedfile.cpp
@@ -9,44 +9,69 @@
 
 #include <fstream>
 #include <iostream>
+#include <cstdio>
 #include <sstream>
 
 #include "Intervals.h"
 #include "ITree.h"
 #include "Bedfile.h"
 
-// Bedfile:  Test I/O for bed3, bed4, bed6, bed12 ?
+class BedfileIOTest: public :: testing::Test {
+  protected:
+  void SetUp() override { }
+  void RunIO(std::string filename) {
+    // Read in the file (both to bedfile object and string stream)
+    sut.load_file(filename);
+    std::ifstream input(filename.c_str()); 
+    original << input.rdbuf(); 
 
-/*
-TEST(Bedfile, ReadWriteEquivalent) 
-{
+    // Now write the file back out and read it that to a second string buffer. 
+    outfn = filename + "_repro";
+    sut.write_file(outfn);
+    std::ifstream second(outfn.c_str()); 
+    duplicate << second.rdbuf(); 
+  }
+  void TearDown() override {
+    std::remove(outfn.c_str()); // Delete the temp file made.
+  }
+  Bedfile sut;
+  std::ostringstream original; 
+  std::string outfn;
+  std::ostringstream duplicate; 
 
-}
-*/
+};
 
 /**
  * @brief Do I write back out the same file I read in.
  * Caveats: doesn't keep comments and writes in chromosome sorted order,
  * so the input file MUST behave under these conditions!
  */
-TEST(Bedfile, ReadWriteCheck) 
+TEST_F(BedfileIOTest, ReadWriteBed4) 
 {
-    std::string filename = "../examples/multiple.nocomments.bed";    //chr21 33401693 33407411	
-    Bedfile sut;
-    sut.load_file(filename);
+    // Arrange
+    std::string filename = "../examples/multiple.nocomments.bed";    //BED4
+    // Act
+    RunIO(filename);
+    //Assert
+    EXPECT_EQ(original.str(), duplicate.str());
+}
 
-    // Read the original file into a buffer called original
-    std::ostringstream original; 
-    std::ifstream input(filename.c_str()); 
-    original << input.rdbuf(); 
+TEST_F(BedfileIOTest, ReadWriteBed6) 
+{
+    // Arrange
+    std::string filename = "../examples/multiple.bed6";    //BED6
+    // Act
+    RunIO(filename);
+    //Assert
+    EXPECT_EQ(original.str(), duplicate.str());
+}
 
-    std::string outfn = "../examples/multiple_repro.bed";    //chr21 33401693 33407411	
-    sut.write_file(outfn);
-
-    // Read the wrote file into a buffer called duplicate 
-    std::ostringstream duplicate; 
-    std::ifstream second(outfn.c_str()); 
-    duplicate << second.rdbuf(); 
-
+TEST_F(BedfileIOTest, ReadWriteBed12) 
+{
+    // Arrange
+    std::string filename = "../examples/multiple.bed12";    //BED6
+    // Act
+    RunIO(filename);
+    //Assert
     EXPECT_EQ(original.str(), duplicate.str());
 }
